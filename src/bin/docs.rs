932a3ca969008ebd05eaa14d1e53bf322c6220a0
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2024-09-26 18:41:00 +0100

    added docs

---
+++ src/bin/docs.rs
@@ -0,0 +1,14 @@
+///Generating documentations
+
+///  A favourite color
+
+enum Color {
+    Red,
+    Blue,
+}
+
+/// A piece of mail
+struct Mail {
+    /// The destintation address
+    address: string,
+}
